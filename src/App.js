4938d5c8d7bf9c5bec019d1fd353c5d622f8bfef
HussainOsama <hussain.osama.asd@gmail.com>
2021-03-13 11:31:10 +0300

    Hussain branch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import "./App.css";
+import "./Hussain.css";
 import AppleWatch from "./components/AppleWatch";
+import Fitness from "./components/Fitness";
 import MacbookAir from "./components/MacBookAir";
+import One from "./components/One";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <h1>Work Space</h1>
-      <div className="container">
+      <div className="container-fluid">
         <AppleWatch />
         <MacbookAir />
       </div>
+      <div className="container-fluid">
+        <Fitness />
+        <One />
+      </div>
+      <Footer></Footer>
     </div>
   );
 }
